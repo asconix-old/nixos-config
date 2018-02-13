4ee888fdd15540de9567de9ef748cb197a2b1b15
Christoph Pilka <30690568+cpilka@users.noreply.github.com>
2018-02-14 00:34:06 +0100

    Added LUKS disk encryption

--- hosts/dorian.nix
+++ hosts/dorian.nix
@@ -3,6 +3,14 @@
 { config, lib, modulesPath, options }:
 
 {
+  boot.initrd.luks.devices = [
+    {
+      name = "root";
+      device = "/dev/disk/by-uuid/e47185ca-2a35-4d2f-8db8-6964a18797b8";                                                                       
+      preLVM = true;
+    }
+  ];
+  
   networking = {
     hostName = "dorian";
   };
